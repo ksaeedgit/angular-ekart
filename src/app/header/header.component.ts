1d6f8d0d35c38e8a613d89123e8a992f01b8d21b
Qalandar <forums.ks@gmail.com>
2024-03-31 11:54:42 -0400

    added header componenet

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-header',
+    template: '<h3>eKart Kashif</h3>',
+    standalone: true
+})
+export class HeaderComponent{
+
+}
